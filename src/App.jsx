2703cb19a36422b66fcd612ea9efeaf074dc05a1
Dcode-7 <151112711+Dcode-7@users.noreply.github.com>
2025-02-01 12:26:12 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 import { Route, Routes } from "react-router-dom";
 import Diet from "./components/diet";
 import Dashboard from "./pages/Dashboard";
 import "./styles/base.css";
+import Tutorials from "./components/Tutorials";
 
-  function App() {
-    return (
-      <Routes>
-        <Route path="/diet" element={<Diet />}></Route>
-        <Route path="/Login" element={<Login />} />
-
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-      </Routes>
-    );
-  }
+function App() {
+  return (
+    <Routes>
+      <Route path="/diet" element={<Diet />}></Route>
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Signup" element={<Signup />} />
+      <Route path="/Tutorials" element={<Tutorials />} />
+      <Route path="/dashboard" element={<Dashboard />}></Route>
+    </Routes>
+  );
+}
 
 export default App;
