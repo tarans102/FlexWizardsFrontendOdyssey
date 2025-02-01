5f4813430147494ae73c61fb101133d2d0a715aa
palashjain2503 <palashjain2503@gmail.com>
2025-02-01 12:25:01 +0530

    diet modification1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes,Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Diet from "./components/diet";
 import Dashboard from "./pages/Dashboard";
 import "./styles/base.css";
